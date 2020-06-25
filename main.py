35b101f03546ad4397e6c088c79b1de94278db66
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-06-25 09:30:50 +0200

    osef

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-sefsef
+lettre = str(input())
+nbLigne = int(input())
+compteur = 0
+total = 0
+while compteur < nbLigne:
+    phrase = str(input())
+    total = total + phrase.count(lettre)
+    compteur = compteur + 1
+
+print(total)
