3e7b650e7a6c210650f0e57d9e59900e96761d10
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-06-26 11:20:03 +0200

    exo convert mesure

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-lettre = str(input())
-nbLigne = int(input())
-compteur = 0
-total = 0
-while compteur < nbLigne:
-    phrase = str(input())
-    total = total + phrase.count(lettre)
-    compteur = compteur + 1
+def convertM(valeur):
+    valeur = float(valeur)
+    result = 3.2808399187 * valeur
+    result = round(result, 6)
+    print(str(result) + " p")
+    
+def convertG(valeur):
+    valeur = float(valeur)
+    result = 0.002205 * valeur
+    result = round(result, 6)
+    print(str(result) + " l")
 
-print(total)
+def convertC(valeur):
+    valeur = float(valeur)
+    result = 32 + 1.8 * valeur
+    result = round(result, 6)
+    print(str(result) + " f")
+    40.354331
+
+
+nbConvert = int(input())
+index = 0
+while index < nbConvert:
+    convertX = str(input())
+    convertX = convertX.split()
+    valeur = convertX[0]
+    mesure = convertX[1]
+    if convertX[1] == "m":
+        convertM(valeur)
+    elif convertX[1] == "c":
+        convertC(valeur)
+    elif convertX[1] == "g":
+        convertG(valeur)
+    index += 1
+
