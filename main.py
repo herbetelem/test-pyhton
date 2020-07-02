fd56a2d07215348dcb081d750511644e4c620852
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-07-02 14:12:46 +0200

    trash

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def count_smileys(arr):
-    count = 0
-    for index in arr:
-        if len(index) == 2:
-            if (index[0] == ":" or index[0] == ";") and (index[1] == ")" or index[1] == "D"):
-                count += 1
-        elif len(index) == 3:
-            if (index[0] == ":" or index[0] == ";") and (index[1] == "-" or index[1] == "~") and (index[2] == ")" or index[2] == "D"):
-                count += 1
-    return count
+def accum(s):
+    liste=[]
+    compteur=0
+    for i in (s):
+        liste.append(i.upper()+i.lower()*compteur)
+        compteur+=1
+    x="-".join(liste)
+    return(x)
 
-print(count_smileys([':D',':~)',';~D',':)']))
+
+print(accum("abcs"))
